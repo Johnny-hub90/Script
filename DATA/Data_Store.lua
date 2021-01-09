c3f08e819e35ec67dba2df4a7f17574d705c6e41
Johnny-hub90 <77188741+Johnny-hub90@users.noreply.github.com>
2021-01-09 16:45:46 +0800

    Update Data_Store.lua

--- DATA/Data_Store.lua
+++ DATA/Data_Store.lua
@@ -1,5 +1,5 @@
 local DataStoreService = game:GetService("DataStoreService")
-local experienceStore = DataStoreService:GetDataStore("PlayerExperience")
+local experienceStore = DataStoreService:GetDataStore("UserExperience")
  
 local success, err = pcall(function()
 	experienceStore:SetAsync("USER_1234", 50)
